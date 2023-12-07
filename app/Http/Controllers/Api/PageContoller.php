b52c6d46ea7fb6fb55407f89e39d4a87d36f77d3
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-12-07 16:16:47 +0100

    add api

---
+++ app/Http/Controllers/Api/PageContoller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PageContoller extends Controller
+{
+    public function index(){
+        $posts = Post::with('category','tags')->paginate(20);
+
+        return response()->json($posts);
+    }
+}
